1d0db98bf4c9297172809b5b0eac6819baa12489
Kartikeya <kartikeya@Kartikeyas-Laptop.local>
2024-09-28 00:49:16 +0400

    Add sample testa and update gitignore

--- test/stream_feed_unofficial_test.dart
+++ test/stream_feed_unofficial_test.dart
@@ -1,1 +1,11 @@
+// test/stream_feed_unofficial_test.dart
+
 // Nothing here as of now
+// Will plan writing tests in next releases
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('dummy test', () {
+    expect(1, equals(1)); // Simple test that will always pass
+  });
+}
